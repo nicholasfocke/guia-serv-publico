c696ba80e4108c84ae78b15b5c78d4f6bde5ab75
nicholasfocke <nicholasfocke05@gmail.com>
2026-04-28 19:18:40 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/guiaserv/publico/GuiaServPublicoApplication.java
@@ -0,0 +1,13 @@
+package com.guiaserv.publico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaServPublicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaServPublicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guiaserv/publico/GuiaServPublicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiaserv.publico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaServPublicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

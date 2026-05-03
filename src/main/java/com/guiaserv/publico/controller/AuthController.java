91be43c49a9d0b09b7a6ab76a581eb52d747e2a4
nicholasfocke <nicholasfocke05@gmail.com>
2026-05-03 20:05:05 -0300

    Correção de duplicação de classe dentro de AuthController

--- src/main/java/com/guiaserv/publico/controller/AuthController.java
+++ src/main/java/com/guiaserv/publico/controller/AuthController.java
@@ -14,24 +14,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    @RestController
-    @RequestMapping("/api/auth")
-    @RequiredArgsConstructor
-    public class AuthController {
 
-        private final AuthService authService;
+    private final AuthService authService;
 
-        @PostMapping("/cadastro")
-        public ResponseEntity<UsuarioResponse> cadastrar(@RequestBody @Valid CadastroUsuarioRequest request) {
-            UsuarioResponse usuario = authService.cadastrar(request);
-            return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
-        }
-
-        @PostMapping("/login")
-        public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
-            LoginResponse response = authService.login(request);
-            return ResponseEntity.ok(response);
-        }
+    @PostMapping("/cadastro")
+    public ResponseEntity<UsuarioResponse> cadastrar(@RequestBody @Valid CadastroUsuarioRequest request) {
+        UsuarioResponse usuario = authService.cadastrar(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
     }
 }
+
